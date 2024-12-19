0233996d964d11217e7de92dafa22918c8b24073
Abisai Ramos <31706095+AbisaiRamos@users.noreply.github.com>
2024-12-19 09:29:25 -0500

    Actualizar bot.js

--- bot.js
+++ bot.js
@@ -1,2 +1,2 @@
 import TelegramBot from "node-telegram-bot-api";
-export const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+export const bot = new TelegramBot(process.env.TOKEN, { polling: false });
