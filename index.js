9c2dc3d6cb0d55b8e31d5d60d42df7c77a1390b3
Abisai Ramos <31706095+AbisaiRamos@users.noreply.github.com>
2024-12-19 10:01:57 -0500

    Actualizar bot.sendmessage

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.post(`/send-message/bot${token}`, (req, res) => {
      bot.processUpdate(req.body);
      console.log(req.body)
      bot.sendMessage(7087786159,req.body)
+         .then(r => {console.log(r)})
+         .catch(err => {console.log(err)}
      res.sendStatus(200); 
 });
 
