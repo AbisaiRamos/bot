dd4eba0a65f02f9209070e7119a53f67c603a21a
Abisai Ramos <blue0802hacker1997@gmail.com>
2024-12-21 16:35:33 -0600

    todo funciona

--- routes/router.js
+++ routes/router.js
@@ -3,10 +3,12 @@ import { login } from "../controller/login.controller.js";
 import { sendMessage } from "../controller/sendMessage.controller.js";
 import { vehicles } from "../controller/vehicles.controller.js";
 import { token } from "../controller/toke.controller.js";
+import { option } from "../controller/option.controller.js";
 
 const expressRouter = Router();
 expressRouter.get("/", vehicles);
 expressRouter.get("/login", login);
 expressRouter.get("/token", token);
+expressRouter.get("/option", option);
 expressRouter.post("/send-message", sendMessage);
 export default expressRouter;
