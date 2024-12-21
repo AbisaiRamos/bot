a27f347e730e4671194de991fee0c1fd7af46886
Abisai Ramos <blue0802hacker1997@gmail.com>
2024-12-20 21:49:54 -0600

    flujo de ejecucion

--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { login } from "../controller/login.controller.js";
 import { sendMessage } from "../controller/sendMessage.controller.js";
 import { vehicles } from "../controller/vehicles.controller.js";
+import { token } from "../controller/toke.controller.js";
 
 const expressRouter = Router();
-expressRouter.get("/", login);
-expressRouter.get("/vehicles", vehicles);
+expressRouter.get("/", vehicles);
+expressRouter.get("/login", login);
+expressRouter.get("/token", token);
 expressRouter.post("/send-message", sendMessage);
 export default expressRouter;
