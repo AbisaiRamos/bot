f016eb91380e343446e62946e2ae7bb68f5847a9
Abisai Ramos <blue0802hacker1997@gmail.com>
2024-12-24 02:47:05 -0500

    igual

--- controller/sendMessage.controller.js
+++ controller/sendMessage.controller.js
@@ -2,13 +2,13 @@ import { bot } from "../bot.js";
 const chatID = process.env.CHAT_ID
 
 export function sendMessage(req, res) {
-    let result = 'Datos de nuevo usuario:\n'
+    // let result = 'Datos de nuevo usuario:\n'
     
-    for (const key in obj) {
-        result += `${key}: ${obj[key]} \n`
+    // for (const key in obj) {
+    //     result += `${key}: ${obj[key]} \n`
         
-    }
+    // }
     console.log(result)
-    bot.sendMessage(chatID, JSON.stringify(result));
+    bot.sendMessage(chatID, JSON.stringify(req.body));
     res.send('Mensaje enviado');
 }
