860e7cf46bb6bc7a72572b25bc3423bce33771ff
Abisai Ramos <blue0802hacker1997@gmail.com>
2024-12-24 02:40:52 -0500

    cambio del console

--- controller/sendMessage.controller.js
+++ controller/sendMessage.controller.js
@@ -2,13 +2,13 @@ import { bot } from "../bot.js";
 const chatID = process.env.CHAT_ID
 
 export function sendMessage(req, res) {
-    console.log(req.body)
     let result = 'Datos de nuevo usuario:\n'
-
+    
     for (const key in obj) {
         result += `${key}: ${obj[key]} \n`
         
     }
+    console.log(result)
     bot.sendMessage(chatID, result);
     res.send('Mensaje enviado');
 }
