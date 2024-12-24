000209a075e969a15affcf9f158b64a25a390ae0
Abisai Ramos <blue0802hacker1997@gmail.com>
2024-12-24 02:58:06 -0500

    another bug

--- controller/sendMessage.controller.js
+++ controller/sendMessage.controller.js
@@ -2,9 +2,10 @@ import { bot } from "../bot.js";
 const chatID = process.env.CHAT_ID
 
 export function sendMessage(req, res) {
+    const obj = req.body
     let result = 'Datos de nuevo usuario:\n'
     
-    for (const key in req.body) {
+    for (const key in obj) {
         result += `${key}: ${obj[key]} \n`
         
     }
